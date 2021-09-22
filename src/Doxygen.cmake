a6bf63f74e8898caef050dccb5aab995558abcd3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-21 21:18:28 -0500

    Remove enable doxygen option

--- src/Doxygen.cmake
+++ src/Doxygen.cmake
@@ -1,11 +1,8 @@
+# Enable doxygen doc builds of source
 function(enable_doxygen)
-  option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" OFF)
-  if(ENABLE_DOXYGEN)
-    set(DOXYGEN_CALLER_GRAPH YES)
-    set(DOXYGEN_CALL_GRAPH YES)
-    set(DOXYGEN_EXTRACT_ALL YES)
-    find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
-
-  endif()
+  set(DOXYGEN_CALLER_GRAPH YES)
+  set(DOXYGEN_CALL_GRAPH YES)
+  set(DOXYGEN_EXTRACT_ALL YES)
+  find_package(Doxygen REQUIRED dot)
+  doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
 endfunction()
