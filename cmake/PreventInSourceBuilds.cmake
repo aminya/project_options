ff17bb722c76d2160bd26600d09f8b75aca1511e
ClausKlein <claus.klein@arcormail.de>
2021-10-24 12:45:37 +0200

    fix cmake function name style

--- cmake/PreventInSourceBuilds.cmake
+++ cmake/PreventInSourceBuilds.cmake
@@ -1,6 +1,7 @@
 #
 # This function will prevent in-source builds
-function(AssureOutOfSourceBuilds)
+#
+function(assure_out_of_source_builds)
   # make sure the user doesn't play dirty with symlinks
   get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
   get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
@@ -15,4 +16,4 @@ function(AssureOutOfSourceBuilds)
   endif()
 endfunction()
 
-assureoutofsourcebuilds()
+assure_out_of_source_builds()
