9d50571f6b030d770af2833e8aa526714f4d9984
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-10-28 23:57:24 -0500

    feat: install vcpkg in ~/vcpkg

--- src/Vcpkg.cmake
+++ src/Vcpkg.cmake
@@ -1,19 +1,43 @@
 include(FetchContent)
 
 macro(run_vcpkg)
-  # Download vcpkg from Github
-  FetchContent_Declare(
-    vcpkg
-    GIT_REPOSITORY "https://github.com/microsoft/vcpkg"
-    GIT_TAG "5568f110b509a9fd90711978a7cb76bae75bb092")
-  FetchContent_MakeAvailable(vcpkg)
+  # optional named VCPKG_DIR argument
+  set(oneValueArgs VCPKG_DIR)
+  cmake_parse_arguments(
+    ProjectOptions
+    ""
+    "${oneValueArgs}"
+    ""
+    ${ARGN})
 
-  # Run vcpkg bootstrap
-  execute_process(COMMAND "./vcpkg/bootstrap-vcpkg" WORKING_DIRECTORY "${vcpkg_SOURCE_DIR}")
+  # Default vcpkg installation directory
+  if(${ProjectOptions_VCPKG_DIR})
+    # the installation directory is specified
+  else()
+    if(WIN32)
+      set(HOME_DIR $ENV{userprofile})
+      set(ProjectOptions_VCPKG_DIR ${HOME_DIR}/vcpkg)
+    else()
+      set(HOME_DIR $ENV{HOME})
+      set(ProjectOptions_VCPKG_DIR ${HOME_DIR}/vcpkg)
+    endif()
+  endif()
+
+  # check if the vcpkg is installed
+  if(EXISTS ${ProjectOptions_VCPKG_DIR})
+    message(STATUS "${ProjectOptions_VCPKG_DIR} already exists. Updating the repository...")
+    execute_process(COMMAND "git" "pull" WORKING_DIRECTORY ${ProjectOptions_VCPKG_DIR})
+  else()
+    message(STATUS "Installing vcpkg at ${ProjectOptions_VCPKG_DIR}")
+    # clone vcpkg from Github
+    execute_process(COMMAND "git" "clone" "https://github.com/microsoft/vcpkg" WORKING_DIRECTORY ${HOME_DIR})
+    # Run vcpkg bootstrap
+    execute_process(COMMAND "./vcpkg/bootstrap-vcpkg" WORKING_DIRECTORY "${ProjectOptions_VCPKG_DIR}")
+  endif()
 
   # Setting up vcpkg toolchain
   list(APPEND VCPKG_FEATURE_FLAGS "versions")
   set(CMAKE_TOOLCHAIN_FILE
-      ${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake
+      ${ProjectOptions_VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake
       CACHE STRING "Vcpkg toolchain file")
 endmacro()
