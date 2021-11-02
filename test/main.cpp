4af0e25bdc0410a3649f33e005724b97dd6d9aaa
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-11-01 20:50:31 -0500

    test: add eigen to the tests

---
+++ test/main.cpp
@@ -0,0 +1,26 @@
+// test external pac
+#include <fmt/core.h>
+#include <Eigen/Dense>
+
+// test std libraries
+#include <iostream>
+#include <string>
+#include <string_view>
+
+// test c libraries
+// #include <cassert>
+// #include <cctype>
+// #include <cstddef>
+// #include <cstring>
+// #include <cstdint>
+
+
+int main() {
+    fmt::print("Hello from fmt{}", "!");
+
+    // populate an Eigen vector with the values
+    auto vec = Eigen::VectorXd::LinSpaced(10, 0, 1);
+
+    // print the vector
+    fmt::print("{}", vec[0]);
+}
