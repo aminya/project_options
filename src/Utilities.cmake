cd6b71b01611c20ce275232e827bc7f98620379f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-11-28 02:55:46 -0600

    feat: add set_env_from_string

--- src/Utilities.cmake
+++ src/Utilities.cmake
@@ -22,3 +22,63 @@ function(
       "${_output}"
       PARENT_SCOPE)
 endfunction()
+
+# A function to set environment variables of CMake from the output of `cmd /c set`
+function(set_env_from_string env_string)
+  # replace ; in paths with __sep__ so we can split on ;
+  string(
+    REGEX
+    REPLACE ";"
+            "__sep__"
+            env_string_sep_added
+            "${env_string}")
+
+  # the variables are separated by \r?\n
+  string(
+    REGEX
+    REPLACE "\r?\n"
+            ";"
+            env_list
+            "${env_string_sep_added}")
+
+  foreach(env_var ${env_list})
+    # split by =
+    string(
+      REGEX
+      REPLACE "="
+              ";"
+              env_parts
+              "${env_var}")
+
+    list(LENGTH env_parts env_parts_length)
+    if("${env_parts_length}" EQUAL "2")
+      # get the variable name and value
+      list(
+        GET
+        env_parts
+        0
+        env_name)
+      list(
+        GET
+        env_parts
+        1
+        env_value)
+
+      # recover ; in paths
+      string(
+        REGEX
+        REPLACE "__sep__"
+                ";"
+                env_value
+                "${env_value}")
+
+      # set env_name to env_value
+      set(ENV{${env_name}} "${env_value}")
+
+      # update cmake program path
+      if("${env_name}" EQUAL "PATH")
+        list(APPEND CMAKE_PROGRAM_PATH ${env_value})
+      endif()
+    endif()
+  endforeach()
+endfunction()
