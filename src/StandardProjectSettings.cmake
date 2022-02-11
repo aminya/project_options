701dace42daafa7476437ab10c037626e2144eb5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-02-11 01:34:12 -0800

    feat: set C standard to the latest

--- src/StandardProjectSettings.cmake
+++ src/StandardProjectSettings.cmake
@@ -42,7 +42,7 @@ endif()
 # if the default CMAKE_CXX_STANDARD is not set detect the latest CXX standard supported by the compiler and use it
 # this is needed for the tools like clang-tidy, cppcheck, etc.
 # Ideally, the user should read the warning and set a default CMAKE_CXX_STANDARD for their project
-# Like not having compiler warnings on by default, this fixes another `bad` defualt for the compilers
+# Like not having compiler warnings on by default, this fixes another `bad` default for the compilers
 if("${CMAKE_CXX_STANDARD}" STREQUAL "")
   if(DEFINED CMAKE_CXX20_STANDARD_COMPILE_OPTION OR DEFINED CMAKE_CXX20_EXTENSION_COMPILE_OPTION)
     set(CXX_LATEST_STANDARD 20)
@@ -58,6 +58,23 @@ if("${CMAKE_CXX_STANDARD}" STREQUAL "")
       "The default CMAKE_CXX_STANDARD used by external targets and tools is not set yet. Using the latest supported C++ standard that is ${CXX_LATEST_STANDARD}"
   )
   set(CMAKE_CXX_STANDARD ${CXX_LATEST_STANDARD})
+endif()
+
+if("{CMAKE_C_STANDARD}" STREQUAL "")
+  if(DEFINED CMAKE_C17_STANDARD_COMPILE_OPTION OR DEFINED CMAKE_C17_EXTENSION_COMPILE_OPTION)
+    set(C_LATEST_STANDARD 17)
+  elseif(DEFINED CMAKE_C11_STANDARD_COMPILE_OPTION OR DEFINED CMAKE_C11_EXTENSION_COMPILE_OPTION)
+    set(C_LATEST_STANDARD 11)
+  elseif(DEFINED CMAKE_C99_STANDARD_COMPILE_OPTION OR DEFINED CMAKE_C99_EXTENSION_COMPILE_OPTION)
+    set(C_LATEST_STANDARD 99)
+  else()
+    set(C_LATEST_STANDARD 90)
+  endif()
+  message(
+    STATUS
+      "The default CMAKE_C_STANDARD used by external targets and tools is not set yet. Using the latest supported C standard that is ${C_LATEST_STANDARD}"
+  )
+  set(CMAKE_C_STANDARD ${C_LATEST_STANDARD})
 endif()
 
 # strongly encouraged to enable this globally to avoid conflicts between
