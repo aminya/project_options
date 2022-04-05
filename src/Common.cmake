ded367ba044100f2add88073772128236600196d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-05 10:04:13 -0700

    feat: add multi-config handling
    Based on
    https://github.com/cpp-best-practices/cpp_boilerplate_project/blob/f37c1abad41705630ddf3a48aec7b6fa17209423/CMakeLists.txt#L52-L72
    
    Co-Authored-By: Jason Turner <234279+lefticus@users.noreply.github.com>

--- src/Common.cmake
+++ src/Common.cmake
@@ -21,6 +21,28 @@ macro(common_project_options)
                "RelWithDebInfo")
   endif()
 
+  get_property(BUILDING_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+  if(BUILDING_MULTI_CONFIG)
+    if(NOT CMAKE_BUILD_TYPE)
+      # Make sure that all supported configuration types have their
+      # associated conan packages available. You can reduce this
+      # list to only the configuration types you use, but only if one
+      # is not forced-set on the command line for VS
+      message(TRACE "Setting up multi-config build types")
+      set(CMAKE_CONFIGURATION_TYPES
+          Debug
+          Release
+          RelWithDebInfo
+          MinSizeRel
+          CACHE STRING "Enabled build types" FORCE)
+    else()
+      message(TRACE "User chose a specific build type, so we are using that")
+      set(CMAKE_CONFIGURATION_TYPES
+          ${CMAKE_BUILD_TYPE}
+          CACHE STRING "Enabled build types" FORCE)
+    endif()
+  endif()
+
   # Generate compile_commands.json to make it easier to work with clang based tools
   set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
