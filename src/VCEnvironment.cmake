a1e8ad74bc35aeb81e5f8d1ba458e18cb0be73f7
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-05-17 22:31:16 -0700

    feat: support mingw with vcpkg

--- src/VCEnvironment.cmake
+++ src/VCEnvironment.cmake
@@ -16,9 +16,8 @@ macro(find_msvc)
          ))
     message(STATUS "Finding MSVC cl.exe ...")
     include(FetchContent)
-    FetchContent_Declare(
-      _msvc_toolchain
-      URL "https://github.com/aminya/Toolchain/archive/3994a752ea395120ffa3cc1c6b1a4cab80527e54.zip")
+    FetchContent_Declare(_msvc_toolchain
+                         URL "https://github.com/aminya/Toolchain/archive/3994a752ea395120ffa3cc1c6b1a4cab80527e54.zip")
     FetchContent_MakeAvailable(_msvc_toolchain)
     include("${_msvc_toolchain_SOURCE_DIR}/Windows.MSVC.toolchain.cmake")
     message(STATUS "Setting CMAKE_CXX_COMPILER to ${CMAKE_CXX_COMPILER}")
