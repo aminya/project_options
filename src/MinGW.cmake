189be55087c2b0c7cab3a7f1d5429c42285ae3d8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-05-18 01:17:47 -0700

    refactor: add is_mingw

--- src/MinGW.cmake
+++ src/MinGW.cmake
@@ -1,7 +1,23 @@
+include_guard()
+
+# detect mingw
+function(is_mingw value)
+  set(_value OFF)
+  if("${MINGW}" STREQUAL "True" OR (WIN32 AND ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"
+                                               OR "${DETECTED_CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")))
+    set(_value ON)
+  endif()
+  set(${value}
+      "${_value}"
+      PARENT_SCOPE)
+endfunction()
+
+# configure mingw toolchain for vcpkg
 macro(configure_mingw_vcpkg)
   # if mingw, use the correct triplet (otherwise it will fail to link libraries)
   include("${ProjectOptions_SRC_DIR}/DetectCompiler.cmake")
-  if(MinGW OR (WIN32 AND "${DETECTED_CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"))
+  is_mingw(_is_mingw)
+  if(${_is_mingw})
     include("${ProjectOptions_SRC_DIR}/Utilities.cmake")
     detect_architecture(_arch)
     string(TOLOWER "${_arch}" _arch)
@@ -18,5 +34,10 @@ macro(configure_mingw_vcpkg)
         CACHE STRING "Default target triplet for vcpkg" FORCE)
     set($ENV{VCPKG_DEFAULT_TRIPLET} "${_arch}-mingw-dynamic")
     set($ENV{VCPKG_DEFAULT_HOST_TRIPLET} "${_arch}-mingw-dynamic")
+
+    message(
+      STATUS
+        "Enabled mingw dynamic toolchain for vcpkg. Make sure that the mingw64/bin directory is on the PATH when running the final executables."
+    )
   endif()
 endmacro()
