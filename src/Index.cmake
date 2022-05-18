4896139d6118b0fb7c431644eba667a0843d6af4
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-05-18 01:14:47 -0700

    refactor: add Mingw.cmake and configure_mingw_vcpkg

--- src/Index.cmake
+++ src/Index.cmake
@@ -30,6 +30,7 @@ include("${ProjectOptions_SRC_DIR}/Sanitizers.cmake")
 include("${ProjectOptions_SRC_DIR}/Doxygen.cmake")
 include("${ProjectOptions_SRC_DIR}/StaticAnalyzers.cmake")
 include("${ProjectOptions_SRC_DIR}/VCEnvironment.cmake")
+include("${ProjectOptions_SRC_DIR}/Mingw.cmake")
 
 # find msvc on windows if required. Should be called before run_vcpkg and run_conan to be effective
 find_msvc()
--- src/Vcpkg.cmake
+++ src/Vcpkg.cmake
@@ -63,26 +63,7 @@ macro(run_vcpkg)
     endif()
   endif()
 
-  # if mingw, use the correct triplet (otherwise it will fail to link libraries)
-  include("${ProjectOptions_SRC_DIR}/DetectCompiler.cmake")
-  if(MinGW OR (WIN32 AND "${DETECTED_CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"))
-    include("${ProjectOptions_SRC_DIR}/Utilities.cmake")
-    detect_architecture(_arch)
-    string(TOLOWER "${_arch}" _arch)
-
-    # Based on this issue, vcpkg uses MINGW variable https://github.com/microsoft/vcpkg/issues/23607#issuecomment-1071966853
-    set(MINGW TRUE)
-
-    # Based on the docs https://github.com/microsoft/vcpkg/blob/master/docs/users/mingw.md (but it doesn't work!)
-    set(VCPKG_DEFAULT_TRIPLET
-        "${_arch}-mingw-dynamic"
-        CACHE STRING "Default triplet for vcpkg" FORCE)
-    set(VCPKG_DEFAULT_HOST_TRIPLET
-        "${_arch}-mingw-dynamic"
-        CACHE STRING "Default target triplet for vcpkg" FORCE)
-    set($ENV{VCPKG_DEFAULT_TRIPLET} "${_arch}-mingw-dynamic")
-    set($ENV{VCPKG_DEFAULT_HOST_TRIPLET} "${_arch}-mingw-dynamic")
-  endif()
+  configure_mingw_vcpkg()
 
   # Setting up vcpkg toolchain
   list(APPEND VCPKG_FEATURE_FLAGS "versions")
