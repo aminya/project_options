6a386ecba716de17acac36c87654b62b9e7401d3
abeimler <alex-beimler@web.de>
2022-07-03 22:29:54 +0200

    fix: add new test lib (#133)

---
+++ test/libs/mythirdpartylib/include/Foo.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+
+#include "mythirdpartylib_export.h"
+
+namespace mythirdpartylib {
+
+class MYTHIRDPARTYLIB_EXPORT Foo {
+public:
+    Foo() = default;
+
+    /*implicit*/ Foo(int a) m_a(a) {}
+
+    int a() const { return m_a; }
+
+    void update(bool b, bool c, bool d);
+    void bad(std::vector<string>& v);
+
+private:
+    int m_a;
+};
+
+}
