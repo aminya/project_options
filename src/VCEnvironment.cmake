8b03a8c0203865fa0559e6b1f2844e93393b602e
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-07-12 03:21:15 -0700

    fix: set VSCMD_DEBUG using cmake

--- src/VCEnvironment.cmake
+++ src/VCEnvironment.cmake
@@ -113,9 +113,13 @@ macro(run_vcvarsall)
     if(EXISTS ${VCVARSALL_FILE})
       # run vcvarsall and print the environment variables
       message(STATUS "Running `${VCVARSALL_FILE} ${VCVARSALL_ARCH}` to set up the MSVC environment")
+
+      # make vcvarsall quiet
+      set(VSCMD_DEBUG "$ENV{VSCMD_DEBUG}")
+      set($ENV{VSCMD_DEBUG} 0)
+
       execute_process(
         COMMAND
-          "set" "VSCMD_DEBUG=0" "&&" # make vcvarsall quiet
           "cmd" "/c" "${VCVARSALL_FILE}" "${VCVARSALL_ARCH}" "1>NUL" #
           "&&" "call" "echo" "VCVARSALL_ENV_START" # a starting point
           "&" "set" # print the environment variables
@@ -123,7 +127,9 @@ macro(run_vcvarsall)
         ERROR_VARIABLE VCVARSALL_ERROR
         OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_STRIP_TRAILING_WHITESPACE)
 
-      if("${VCVARSALL_ERROR}" STREQUAL "")
+      # recover VSCMD_DEBUG variable
+      set($ENV{VSCMD_DEBUG} "${VSCMD_DEBUG}")
+
         # parse the output and get the environment variables string
         find_substring_by_prefix(VCVARSALL_ENV "VCVARSALL_ENV_START" "${VCVARSALL_OUTPUT}")
 
