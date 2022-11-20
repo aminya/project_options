9fca801f0fa5c902f4111d58f3d03d25f41487ef
abeimler <alex-beimler@web.de>
2022-11-20 02:31:37 +0100

    feat: use taskfile for docker commands

--- docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-
-cmake -B . -G "Ninja" -DCMAKE_BUILD_TYPE:STRING=Release ..
-cmake --build . --config Release
--- docker/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-
-cmake -B . -G "Ninja" -DCMAKE_BUILD_TYPE:STRING=Debug ..
-cmake --build . --config Debug
-ctest -C Debug --verbose
