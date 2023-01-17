9983f837f2dc22b1f3579149ad2f7cd97b360732
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-01-16 21:39:29 -0800

    test: fix the Windows specific tasks

--- tests/myproj/src/main/main.cpp
+++ tests/myproj/src/main/main.cpp
@@ -16,20 +16,20 @@
 #include <cstring>
 
 int main() {
-  fmt::print("Hello from fmt{}", "!");
+    fmt::print("Hello from fmt{}", "!");
 
-  Eigen::VectorXd eigen_vec = Eigen::Vector3d(1, 2, 3);
-  fmt::print("{}", eigen_vec);
+    Eigen::VectorXd eigen_vec = Eigen::Vector3d(1, 2, 3);
+    fmt::print("{}", eigen_vec);
 
-#if !defined(__MINGW32__) && !defined(__MSYS__) // TODO fails
-  Eigen::VectorXd eigen_vec2 = Eigen::VectorXd::LinSpaced(10, 0, 1);
-  fmt::print("{}", eigen_vec2);
+#if !defined(__MINGW32__) && !defined(__MSYS__)// TODO fails
+    Eigen::VectorXd eigen_vec2 = Eigen::VectorXd::LinSpaced(10, 0, 1);
+    fmt::print("{}", eigen_vec2);
 #endif
 
-  // trigger address sanitizer
-  // int *p = nullptr;
-  // *p = 1;
+    // trigger address sanitizer
+    // int *p = nullptr;
+    // *p = 1;
 
-  // trigger compiler warnings, clang-tidy, and cppcheck
-  int a;
+    // trigger compiler warnings, clang-tidy, and cppcheck
+    int a;
 }
