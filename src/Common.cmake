7e23a2d699e3b4b4a92573830f5289206a3b850c
FeignClaims <www4112379@163.com>
2023-01-30 12:32:08 +0800

    Fix misorder of compile_commands.json paths

--- src/Common.cmake
+++ src/Common.cmake
@@ -88,13 +88,13 @@ macro(common_project_options)
 
     if(IS_NONADMINISTRATOR)
       # For non-administrator, create an auxiliary target and ask user to run it
-      add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/compile_commands.json
+      add_custom_command(OUTPUT ${CMAKE_SOURCE_DIR}/compile_commands.json
           COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/compile_commands.json ${CMAKE_SOURCE_DIR}/compile_commands.json
-          DEPENDS ${CMAKE_SOURCE_DIR}/compile_commands.json
+          DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
           VERBATIM
       )
       add_custom_target(_copy_compile_commands
-          DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
+          DEPENDS ${CMAKE_SOURCE_DIR}/compile_commands.json
           VERBATIM
       )
       message(STATUS "compile_commands.json was not symlinked to the root. Run `cmake --build <build_dir> -t _copy_compile_commands` if needed.")
@@ -103,13 +103,13 @@ macro(common_project_options)
       file(CREATE_LINK ${CMAKE_BINARY_DIR}/compile_commands.json ${CMAKE_SOURCE_DIR}/compile_commands.json SYMBOLIC)
 
       # create an auxiliary target to help users switch confiurations
-      add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/compile_commands.json
+      add_custom_command(OUTPUT ${CMAKE_SOURCE_DIR}/compile_commands.json
           COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/compile_commands.json ${CMAKE_SOURCE_DIR}/compile_commands.json
-          DEPENDS ${CMAKE_SOURCE_DIR}/compile_commands.json
+          DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
           VERBATIM
       )
       add_custom_target(_copy_compile_commands
-          DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
+          DEPENDS ${CMAKE_SOURCE_DIR}/compile_commands.json
           VERBATIM
       )
       message(STATUS "compile_commands.json was symlinked to the root. Run `cmake --build <build_dir> -t _copy_compile_commands` if switched among configurations.")
