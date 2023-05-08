361d338a08524110b247a9a794395b099dd18797
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-05-08 13:40:54 -0700

    fix: fix regex match for git_parse_url

--- src/Git.cmake
+++ src/Git.cmake
@@ -6,63 +6,79 @@ include_guard()
 # `[protocol://][host]/user/repo[.git]`
 #
 # Input variables:
-# - URL: The url to parse
+# - INPUT_URL: The url to parse
 #
 # Output variables:
-# - REMOTE_PROTOCOL: The protocol of the url (http, https, ssh, etc)
-# - REMOTE_HOST: The host of the url (github, gitlab, etc)
-# - REMOTE_USER: The user of the url (username, organization, etc)
-# - REMOTE_REPOSITORY_NAME: The repository of the url (project name)
-# - REMOTE_FULL_URL: The url of the repository (protocol + host + user + repo)
+# - PROTOCOL: The protocol of the url (http, https, ssh, etc)
+# - HOST: The host of the url (github, gitlab, etc)
+# - USER: The user of the url (username, organization, etc)
+# - REPOSITORY_NAME: The repository of the url (project name)
+# - FULL_URL: The url of the repository (protocol + host + user + repo)
 function(
   git_parse_url
   INPUT_URL
   # output variables
-  REMOTE_PROTOCOL
-  REMOTE_HOST
-  REMOTE_USER
-  REMOTE_REPOSITORY_NAME
-  REMOTE_FULL_URL)
-  # https://regex101.com/r/jfU0cz/1
+  PROTOCOL
+  HOST
+  USER
+  REPOSITORY_NAME
+  FULL_URL)
+  # https://regex101.com/r/gVep0l/1
   string(
     REGEX MATCH
-          "([a-z]+://)?([^/]*)/([^/]*)/(.*)(\\.git)?"
-          _
-          ${INPUT_URL})
-  set(REMOTE_PROTOCOL ${CMAKE_MATCH_1})
-  set(REMOTE_HOST ${CMAKE_MATCH_2})
-  set(REMOTE_USER ${CMAKE_MATCH_3})
-  set(REMOTE_REPOSITORY_NAME ${CMAKE_MATCH_4})
+          "([a-z]+:\/\/)?(.*\/)?([^/]*)\/(.*)"
+          _matched
+          "${INPUT_URL}")
+  if(NOT
+     "${_matched}"
+     STREQUAL
+     "${INPUT_URL}")
+    message(SEND_ERROR "Could not parse git url: ${URL}")
+    return()
+  endif()
+  set(_PROTOCOL ${CMAKE_MATCH_1})
+  set(_HOST ${CMAKE_MATCH_2})
+  set(_USER ${CMAKE_MATCH_3})
+  set(_REPOSITORY_NAME ${CMAKE_MATCH_4})
 
-  if(NOT REMOTE_USER OR NOT REMOTE_REPOSITORY_NAME)
+  if(NOT _USER OR NOT _REPOSITORY_NAME)
     message(SEND_ERROR "Could not parse git url: ${URL}")
     return()
   endif()
 
-  if(NOT REMOTE_PROTOCOL)
-    set(REMOTE_PROTOCOL "https://")
+  if(NOT _PROTOCOL)
+    set(_PROTOCOL "https://")
   endif()
 
-  if(NOT REMOTE_HOST)
-    set(REMOTE_HOST "github.com")
+  if(NOT _HOST)
+    set(_HOST "github.com")
   endif()
 
-  set(REMOTE_FULL_URL "${REMOTE_PROTOCOL}${REMOTE_HOST}/${REMOTE_USER}/${REMOTE_REPOSITORY_NAME}.git")
+  # strip .git from the end of the repository name
+  string(
+    REGEX
+    REPLACE "\.git$"
+            ""
+            _REPOSITORY_NAME
+            "${_REPOSITORY_NAME}")
 
-  set(${REMOTE_PROTOCOL}
-      ${REMOTE_PROTOCOL}
+  # construct the full url
+  set(_FULL_URL "${_PROTOCOL}${_HOST}/${_USER}/${_REPOSITORY_NAME}.git")
+
+  set(${PROTOCOL}
+      ${_PROTOCOL}
       PARENT_SCOPE)
-  set(${REMOTE_HOST}
-      ${REMOTE_HOST}
+  set(${HOST}
+      ${_HOST}
       PARENT_SCOPE)
-  set(${REMOTE_USER}
-      ${REMOTE_USER}
+  set(${USER}
+      ${_USER}
       PARENT_SCOPE)
-  set(${GIT_REPO}
-      ${GIT_REPO}
+  set(${REPOSITORY_NAME}
+      ${_REPOSITORY_NAME}
       PARENT_SCOPE)
-  set(${REMOTE_FULL_URL}
-      ${REMOTE_FULL_URL}
+  set(${FULL_URL}
+      ${_FULL_URL}
       PARENT_SCOPE)
 endfunction()
 
@@ -81,12 +97,8 @@ function(git_add_remote)
     ""
     ${ARGN})
 
-  if("${_fun_REPOSITORY_PATH}" STREQUAL "")
-    message(FATAL_ERROR "REPOSITORY_PATH is required")
-  endif()
-
-  if("${_fun_REMOTE_URL}" STREQUAL "")
-    message(FATAL_ERROR "REMOTE_URL is required")
+  if("${_fun_REPOSITORY_PATH}" STREQUAL "" OR "${_fun_REMOTE_URL}" STREQUAL "")
+    message(FATAL_ERROR "REPOSITORY_PATH and REMOTE_URL are required")
   endif()
 
   find_program(GIT_EXECUTABLE "git" REQUIRED)
@@ -104,12 +116,12 @@ function(git_add_remote)
       # use the remote user as the remote name if it's not given
       git_parse_url(
         "${_fun_REMOTE_URL}"
-        _REMOTE_PROTOCOL
-        _REMOTE_HOST
-        _REMOTE_USER
-        _REMOTE_REPOSITORY_NAME
-        _REMOTE_FULL_URL)
-      set(_fun_REMOTE_NAME "${_REMOTE_USER}")
+        _PROTOCOL
+        _HOST
+        _USER
+        _REPOSITORY_NAME
+        _FULL_URL)
+      set(_fun_REMOTE_NAME "${_USER}")
     endif()
 
     execute_process(COMMAND "${GIT_EXECUTABLE}" "remote" "add" "${_fun_REMOTE_NAME}" "${_fun_REMOTE_URL}"
@@ -133,6 +145,10 @@ function(git_clone)
     "${oneValueArgs}"
     ""
     ${ARGN})
+
+  if("${_fun_REPOSITORY_PATH}" STREQUAL "" OR "${_fun_REMOTE_URL}" STREQUAL "")
+    message(FATAL_ERROR "REPOSITORY_PATH and _fun_REMOTE_URL are required")
+  endif()
 
   if(NOT EXISTS "${_fun_REPOSITORY_PATH}")
     message(STATUS "Cloning at ${_fun_REPOSITORY_PATH}")
@@ -166,6 +182,10 @@ function(git_switch_back)
     ""
     ${ARGN})
 
+  if("${_fun_REPOSITORY_PATH}" STREQUAL "")
+    message(FATAL_ERROR "REPOSITORY_PATH is required")
+  endif()
+
   set(_git_status "")
   find_program(GIT_EXECUTABLE "git" REQUIRED)
   execute_process(
@@ -193,6 +213,10 @@ function(git_pull)
     "${oneValueArgs}"
     ""
     ${ARGN})
+
+  if("${_fun_REPOSITORY_PATH}" STREQUAL "")
+    message(FATAL_ERROR "REPOSITORY_PATH is required")
+  endif()
 
   git_switch_back(REPOSITORY_PATH "${_fun_REPOSITORY_PATH}")
 
